4900efc55824a0c856786c4957786cfef3fdea35
Joe McGarry <joewmcgarry@gmail.com>
2022-09-05 14:10:40 +0100

    Adds test for adding multiple items to items array

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -13,4 +13,13 @@ describe GildedRose do
     expect(gilded_rose.items).to eq [fake_item]
   end
 
+  it "should add multiple items to the array" do
+    fake_banana = double :fake_item, name: "Banana", sell_in: 5, quality: 10
+    fake_apple = double :fake_item, name: "Apple", sell_in: 5, quality: 10
+    gilded_rose = GildedRose.new
+    gilded_rose.add_item(fake_banana)
+    gilded_rose.add_item(fake_apple)
+    expect(gilded_rose.items).to eq [fake_banana, fake_apple]
+  end
+
 end
