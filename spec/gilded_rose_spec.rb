226a23630ea965d75b155c9ed2fed79d7de8f39e
Joe McGarry <joewmcgarry@gmail.com>
2022-09-05 13:54:42 +0100

    Adds test for adding items to items array

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,4 +6,11 @@ describe GildedRose do
     expect(gilded_rose.items).to eq []
   end
 
+  it "can add items to the array" do
+    fake_item = double :fake_item, name: "Banana", sell_in: 5, quality: 10 
+    gilded_rose = GildedRose.new
+    gilded_rose.add_item(fake_item)
+    expect(gilded_rose.items).to eq [fake_item]
+  end
+
 end
