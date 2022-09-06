2d0b37574b3277771af439ecddf408f71cf94f08
Joe McGarry <joewmcgarry@gmail.com>
2022-09-06 13:37:46 +0100

    Refactors code from GildedRose, updates NormalItem class for increasing quality reduction

--- lib/normal_item.rb
+++ lib/normal_item.rb
@@ -1,6 +1,10 @@
 class NormalItem < Item
     def update_item
-        @sell_in -= 1
-        @quality -= 1
+        if @sell_in == 0
+            @quality -= 2
+        else
+            @sell_in -= 1
+            @quality -= 1
+        end
     end
 end
